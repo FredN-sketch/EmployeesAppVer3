b181e793ae4db72891c4e5ab07742f6e202081ad
Fredrik Norgren <fredrik_norgren@hotmail.com>
2025-05-21 11:24:56 +0200

    Service-controller ickestatisk

--- EmployeesApp.Web/Program.cs
+++ EmployeesApp.Web/Program.cs
@@ -1,3 +1,5 @@
+using EmployeesApp.Web.Services;
+
 namespace EmployeesApp.Web
 {
     //test
@@ -7,6 +9,7 @@ namespace EmployeesApp.Web
         {
             var builder = WebApplication.CreateBuilder(args);
             builder.Services.AddControllersWithViews();
+            builder.Services.AddSingleton<EmployeeService>();
             var app = builder.Build();
             app.UseStaticFiles();
             app.MapControllers();
